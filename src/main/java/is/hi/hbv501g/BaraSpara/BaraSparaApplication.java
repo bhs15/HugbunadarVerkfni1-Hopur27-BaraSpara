61ed1c988a39a7a74e1c2df9e5c4efd3347e3650
THWinther <43626565+THWinther@users.noreply.github.com>
2020-10-01 11:26:51 +0000

    Initial commit

---
+++ src/main/java/is/hi/hbv501g/BaraSpara/BaraSparaApplication.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.BaraSpara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaraSparaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaraSparaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/is/hi/hbv501g/BaraSpara/BaraSparaApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.BaraSpara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaraSparaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
