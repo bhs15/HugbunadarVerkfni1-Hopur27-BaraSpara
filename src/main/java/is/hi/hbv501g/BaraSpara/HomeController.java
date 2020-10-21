e4c0577c77a348e5fed61b6947f4e7ebd56a8a98
THWinther <43626565+THWinther@users.noreply.github.com>
2020-10-21 01:00:15 +0000

    gera fall til að eyða eyðslu

--- src/main/java/is/hi/hbv501g/BaraSpara/HomeController.java
+++ src/main/java/is/hi/hbv501g/BaraSpara/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -36,7 +37,24 @@ public class HomeController {
 
         //Vistar nýju SavingType og skilar nýja listanum með model
         savingTypeService.save(savingType);
-        model.addAttribute("savingType", savingTypeService.findAll());
+        model.addAttribute("savingTypes", savingTypeService.findAll());
+        return "Velkominn";
+    }
+
+    //TODO error handling og HTML síðu
+    @RequestMapping(value = "/addSavingType", method = RequestMethod.GET)
+    public String addSavingTypeForm(Model model){
+        return "add-SavingType";
+    }
+
+    //TODO HTML síðu /delete
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteSavingType(@PathVariable("id") long id, Model model){
+        SavingType sType = savingTypeService.findById(id).orElseThrow(
+                ()-> new IllegalArgumentException("Invalid SavingTypeID"));
+
+        savingTypeService.delete(sType);
+        model.addAttribute("savingTypes", savingTypeService.findAll());
         return "Velkominn";
     }
 
